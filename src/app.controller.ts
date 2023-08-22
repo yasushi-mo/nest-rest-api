9dccc910753c4b3d4d2c0a864c559a2bff8b5475
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-08-23 08:59:33 +0900

    Add:test routing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller('greetings')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello!';
+  }
+
+  @Get('good-morning')
+  getHi(): string {
+    return 'Good morning!';
   }
 }
+
+// import { AppService } from './app.service';
+
+// constructor(private readonly appService: AppService) {}
+
+// @Get()
+// getHello(): string {
+//   return this.appService.getHello();
+// }
