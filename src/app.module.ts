05efb2b2e16cd51cbdedc6d34215e4bdd4f6b045
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-09-07 09:54:59 +0900

    Add:register cats service & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './controllers/cats.controller';
+import { CatsService } from './services/cats.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CatsController],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
