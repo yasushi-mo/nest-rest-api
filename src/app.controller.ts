9349dd329e36f891b932eb07b705d211177c05d1
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-09-14 10:11:23 +0900

    Add:request object

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,7 +17,7 @@ export class AppController {
     return 'This route uses a wildcard';
   }
 
-  @Get(':name')
+  @Get('/hello/:name')
   getHelloWithName(@Param('name') name: string): string {
     console.log(name);
     return `Hello, ${name}!`;
@@ -28,6 +28,22 @@ export class AppController {
     console.log(params);
     return `Good morning, ${params.name}!`;
   }
+
+  // @Get('good-afternoon/request')
+  // getGoodAfternoonRequest(@Req() request: Request): string {
+  //   console.log(request);
+  //   return `${request}`;
+  // }
+
+  // @Get('good-afternoon/query')
+  // getGoodAfternoonQuery(@Req() request: Request): string {
+  //   return `${request}`;
+  // }
+
+  // @Post('good-afternoon/body')
+  // postGoodAfternoon(@Body('name') name: string): string {
+  //   return `Good afternoon, ${name}!`;
+  // }
 }
 
 // import { AppService } from './app.service';
