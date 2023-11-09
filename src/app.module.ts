46f01456f4b867d64fc0131b37d486de6d0c5491
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-11-10 08:21:37 +0900

    Add:exports module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './modules/cats.module';
+import { CatsService } from './services/cats.service';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { CatsService } from './services/cats.service';
 
 @Injectable()
 export class AppService {
+  constructor(private catsService: CatsService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
