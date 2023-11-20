36a9edbbe5f3a16c94e4d4011b8ebddfbdcbb6bf
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-11-20 09:46:13 +0900

    Add:TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './modules/cats.module';
 import { CatsService } from './services/cats.service';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3001,
+      username: 'root',
+      password: 'password',
+      database: 'blog_app',
+      entities: [],
+      synchronize: true,
+      retryAttempts: 3,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, CatsService],
 })
