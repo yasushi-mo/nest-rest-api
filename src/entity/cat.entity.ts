3607a5b7f564465749b080c41612cc95eed43818
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-11-28 09:40:51 +0900

    Add:cat entity

---
+++ src/entity/cat.entity.ts
@@ -0,0 +1,15 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Cat {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  breed: string;
+}
