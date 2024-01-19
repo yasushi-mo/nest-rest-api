062197394605fb31b6781ba25106c812a41df8b6
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-01-20 07:22:32 +0900

    Add:validator for dto

--- src/cats/cat.dto.ts
+++ src/cats/cat.dto.ts
@@ -1,12 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsString, Length } from "class-validator";
 
 export class CatDTO {
   @ApiProperty()
+  @IsString()
+  @Length(1, 30)
   name: string;
 
   @ApiProperty()
+  @IsInt()
   age: number;
 
   @ApiProperty()
+  @IsString()
+  @Length(1, 30)
   breed: string;
 }
