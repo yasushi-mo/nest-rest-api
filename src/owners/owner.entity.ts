6162153d33904b03b7eab5250e5bfe5e48c2e2bd
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-02-07 10:35:08 +0900

    Add:relation

--- src/owners/owner.entity.ts
+++ src/owners/owner.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Cat } from "src/cats/cat.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("owners")
 export class Owner {
@@ -17,4 +18,7 @@ export class Owner {
     nullable: false,
   })
   age: number;
+
+  @OneToMany(() => Cat, (cat) => cat.owner)
+  cats: Cat[];
 }
